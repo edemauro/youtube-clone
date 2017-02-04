1e199774280af77f2555a7b69ae9f6716f5d8a14
Eric Demauro <erickdemauro@gmail.com>
2017-02-03 21:46:52 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = process.env.API_KEY;
-
-YTSearch({key: API_KEY, term: 'dogs'}, (data) => {
-  console.log(data);
-});
 
 class App extends Component {
   render() {
