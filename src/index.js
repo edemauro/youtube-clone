8223aab9b73cb6b55e62e153313879522d3c80ac
Eric Demauro <erickdemauro@gmail.com>
2017-02-03 22:16:23 -0800

    added axios to make youtube http request.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import axios from 'axios';
 
+const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
 const API_KEY = process.env.API_KEY;
+
+let params = {
+  part: 'snippet',
+  key: API_KEY,
+  q: 'dogs'
+};
+
+axios.get('https://www.googleapis.com/youtube/v3/search', { params })
+  .then((res) => { console.log(res); })
+  .catch((err) => { console.log(err); })
 
 class App extends Component {
   render() {
